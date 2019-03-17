53527dcc14c7f906501736d63eaa69146c1886ec
marcelocampana <46510921+marcelocampana@users.noreply.github.com>
2019-03-16 21:48:42 -0300

    Update weatherSummary.js

--- lesson-10/scripts/weatherSummary.js
+++ lesson-10/scripts/weatherSummary.js
@@ -1,5 +1,5 @@
 var weatherObj = new XMLHttpRequest();
-weatherObj.open ('GET','http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=8dc7d24dc0c4e4babe471e12034b5a39', true)
+weatherObj.open ('GET','https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=8dc7d24dc0c4e4babe471e12034b5a39', true)
 weatherObj.send();
 
 weatherObj.onload = function(){
