8ef664e1499aa3cc82c734587f1ba789661db91b
marcelocampana <cam17010@byui.edu>
2019-04-10 10:42:48 -0400

    1

--- termsite/scripts/sacramento_temple.js
+++ termsite/scripts/sacramento_temple.js
@@ -3,7 +3,15 @@ var xmlhttp = new XMLHttpRequest();
         if (this.readyState == 4 && this.status == 200) {
             var myArr = JSON.parse(this.responseText);
 
-            document.getElementById("title_temple_sac").innerHTML = myArr.temple[0].name;
+            document.getElementById("Name_temple_sac").innerHTML = myArr.temple[0].Name;
+            document.getElementById("Address_temple_sac").innerHTML = myArr.temple[0].Address;
+            document.getElementById("Telephone_temple_sac").innerHTML = myArr.temple[0].Telephone;
+            document.getElementById("Services1_temple_sac").innerHTML = myArr.temple[0].Services[0];
+            document.getElementById("Services2_temple_sac").innerHTML = myArr.temple[0].Services[1];
+            document.getElementById("Services3_temple_sac").innerHTML = myArr.temple[0].Services[2];
+            document.getElementById("Services4_temple_sac").innerHTML = myArr.temple[0].Services[3];
+           
+
    
             }
         }
