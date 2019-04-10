19cd95c1816b357357487c7ad023bdc8a2959981
marcelocampana <cam17010@byui.edu>
2019-04-10 11:10:49 -0400

    1

--- termsite/scripts/sacramento_temple.js
+++ termsite/scripts/sacramento_temple.js
@@ -4,6 +4,7 @@ var xmlhttp = new XMLHttpRequest();
             var myArr = JSON.parse(this.responseText);
 
             document.getElementById("Name_temple_sac").innerHTML = myArr.temple[0].Name;
+            document.getElementById("Name_temple_sac_btn").innerHTML = myArr.temple[0].Name;
             document.getElementById("Address_temple_sac").innerHTML = myArr.temple[0].Address;
             document.getElementById("Telephone_temple_sac").innerHTML = myArr.temple[0].Telephone;
             document.getElementById("Services1_temple_sac").innerHTML = myArr.temple[0].Services[0];
@@ -12,6 +13,20 @@ var xmlhttp = new XMLHttpRequest();
             document.getElementById("Services4_temple_sac").innerHTML = myArr.temple[0].Services[3];
             document.getElementById("Baptistry_Schedule1_temple_sac").innerHTML = myArr.temple[0].Baptistry_Schedule[0];
             document.getElementById("Baptistry_Schedule2_temple_sac").innerHTML = myArr.temple[0].Baptistry_Schedule[1];
+            document.getElementById("Endowment_Schedule1_temple_sac").innerHTML = myArr.temple[0].Endowment_Schedule[0];
+            document.getElementById("Endowment_Schedule2_temple_sac").innerHTML = myArr.temple[0].Endowment_Schedule[1];
+            document.getElementById("Endowment_Schedule3_temple_sac").innerHTML = myArr.temple[0].Endowment_Schedule[2];
+            document.getElementById("Endowment_Schedule4_temple_sac").innerHTML = myArr.temple[0].Endowment_Schedule[3];
+            document.getElementById("Temple_Closures1_temple_sac").innerHTML = myArr.temple[0].Temple_Closures[0];
+            document.getElementById("Temple_Closures2_temple_sac").innerHTML = myArr.temple[0].Temple_Closures[1];
+            document.getElementById("Temple_Closures3_temple_sac").innerHTML = myArr.temple[0].Temple_Closures[2];
+            document.getElementById("Temple_Closures4_temple_sac").innerHTML = myArr.temple[0].Temple_Closures[3];
+            document.getElementById("Temple_Closures5_temple_sac").innerHTML = myArr.temple[0].Temple_Closures[4];
+            document.getElementById("Temple_Closures6_temple_sac").innerHTML = myArr.temple[0].Temple_Closures[5];
+            document.getElementById("Temple_Closures7_temple_sac").innerHTML = myArr.temple[0].Temple_Closures[6];
+            document.getElementById("Milestones1_temple_sac").innerHTML = myArr.temple[0].Endowment_Schedule[0];
+            document.getElementById("Milestones2_temple_sac").innerHTML = myArr.temple[0].Endowment_Schedule[1];
+            document.getElementById("Milestones3_temple_sac").innerHTML = myArr.temple[0].Endowment_Schedule[2];
 
            
 
